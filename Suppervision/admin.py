d70d0bd9c7e1df782a870ddc63f6640b195a2f9b
Tyrone2ki <gastephane0@gmail.com>
2024-05-06 10:14:16 +0200

    Back_end commit w/o Fuctions

---
+++ Suppervision/admin.py
@@ -0,0 +1,75 @@
+from typing import Any
+from django.contrib import admin
+from django.db.models.query import QuerySet
+from .models import *
+
+
+admin.site.site_header='RENOVATION IMMOBILIERE'
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = "user","age"
+
+@admin.register(Design)
+class DesignAdmin(admin.ModelAdmin):
+    list_display =  ("nom",)
+
+@admin.register(CategoryDesign)
+class CategoryDesignAdmin(admin.ModelAdmin):
+    list_display =  ("nom",)
+
+@admin.register(CategoryProduit)
+class CategoryProduitAdmin(admin.ModelAdmin):
+    list_display =  ('nom',)
+
+@admin.register(TypePaiement)
+class TypePaiementAdmin(admin.ModelAdmin):
+    list_display =  ("nom",)    
+
+
+@admin.register(Produit)
+class ProduitAdmin(admin.ModelAdmin):
+    list_display = "nom", "type", "unite", "quantite", "prix"
+
+
+@admin.register(Fournisseur)
+class FournisseurAdmin(admin.ModelAdmin):
+    list_display = "user", "adresse", "contact", "description" 
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = "created_by", "produit", "fournisseur", "quantite_initiale", "quantite_actuelle",
+    "delais_expiration", "prix" 
+
+
+@admin.register(Commande)
+class CommandeAdmin(admin.ModelAdmin):
+    list_display = "created_by", "category", "budget"
+
+
+@admin.register(ProduitCommande)
+class ProduitCommandeAdmin(admin.ModelAdmin):
+    list_display = "commande", "produit", "design", "quantite", "prix"
+
+
+@admin.register(Paiement)
+class PaiementAdmin(admin.ModelAdmin):
+    list_display = "type_paiement", "montant", "created_by", "commande"
+
+
+@admin.register(ListeAttente)
+class ListeAttenteAdmin(admin.ModelAdmin):
+    list_display = "created_by", "client", "done"
+
+
+@admin.register(SuppervisionTravaux)
+class SuppervisionTravauxAdmin(admin.ModelAdmin):
+    list_display = "client", "budget1", "description"
+
+
+@admin.register(RenovationFaite)
+class RenovationFaiteAdmin(admin.ModelAdmin):
+    list_display = "client", "budget2", "description"
+# Register your models here.
---
+++ Suppervision/models.py
@@ -0,0 +1,147 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Client(models.Model):
+    id = models.AutoField(primary_key=True)
+    age = models.PositiveIntegerField()
+    user = models.OneToOneField(User,null=True, on_delete=models.CASCADE)
+
+    def __str__(self) :
+        return f"{self.user.username}"
+    
+class Design(models.Model):
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=50)   
+
+    def __str__(self) :
+        return f"{self.nom}"
+    
+class CategoryDesign(models.Model):
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=50)
+
+    def __str__(self) :
+        return f"{self.nom}"
+
+
+class CategoryProduit(models.Model):
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=50)        
+
+    def __str__(self) :
+        return f"{self.nom}"
+
+class TypePaiement(models.Model):
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=30, null=True)   
+
+    def __str__(self) :
+        return f"{self.nom}"    
+    
+
+class Produit(models.Model):
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=50)
+    type = models.ForeignKey(CategoryProduit, null=True, on_delete=models.CASCADE) 
+    unite = models.CharField(max_length=31)
+    quantite = models.FloatField(editable=False, null=True)
+    prix = models.FloatField()
+
+    def __str__(self) :
+        return f"{self.nom} {self.type} de {self.prix}"
+
+class Fournisseur(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User,max_length=30,null=True, on_delete=models.CASCADE )
+    adresse = models.CharField(max_length=63)
+    contact = models.BigIntegerField(null=True)
+    description = models.CharField(max_length=128)
+
+    def __str__(self) :
+        return f"{self.user.username}"
+    
+class Stock(models.Model):
+    id = models.AutoField(primary_key=True)
+    created_by = models.ForeignKey(User, on_delete=models.PROTECT)
+    produit = models.ForeignKey(Produit, on_delete=models.CASCADE)
+    fournisseur = models.ForeignKey(Fournisseur, on_delete=models.PROTECT)
+    quantite_initiale = models.FloatField(default=0)
+    quantite_actuelle = models.FloatField(editable=False, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    delais_expiration = models.PositiveIntegerField(null=True, blank=True)
+    prix = models.FloatField()
+
+    def __str__(self) :
+        return f"{self.fournisseur.user.username} nous a founit {self.produit.nom} à {self.prix}"
+    
+class Commande(models.Model):
+    id = models.AutoField(primary_key=True)
+    created_by = models.ForeignKey(Client, on_delete=models.PROTECT, null=True)
+    category = models.ForeignKey(Design,null=True, on_delete=models.SET_NULL)
+    budget = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    longitude = models.FloatField(default=0, editable=False)
+    latitude = models.FloatField(default=0, editable=False)
+
+    def __str__(self) :
+        return f"Commande de {self.created_by.user.username} {self.category} valant {self.budget}"
+        
+class ProduitCommande(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    produit = models.ForeignKey(Produit, on_delete=models.PROTECT)
+    commande = models.ForeignKey(Commande, null=True, on_delete=models.SET_NULL)
+    design = models.ForeignKey(CategoryDesign, null=True, on_delete=models.CASCADE)
+    quantite = models.FloatField(editable=False,default=0)
+    prix = models.FloatField(editable=False,default=0)
+
+    def __str__(self) :
+        return f"{self.commande.created_by.user.username} {self.quantite} {self.produit.unite} de {self.produit}"
+    
+    class Meta:
+        verbose_name = "Panier"
+        verbose_name_plural = "Panier"
+
+ 
+class Paiement(models.Model):
+    id = models.AutoField(primary_key=True)
+    type_paiement = models.ForeignKey(TypePaiement,null=True, on_delete=models.PROTECT)
+    montant = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.PROTECT)
+    commande = models.ForeignKey(Commande, on_delete=models.CASCADE)
+
+    def __str__(self) :
+        return f"Les frais de {self.type_paiement} de {self.montant} sur {self.commande} à {self.created_by}"
+
+class ListeAttente(models.Model):
+    id = models.AutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.PROTECT)
+    client = models.ForeignKey(Client, null=True, on_delete=models.PROTECT)
+    done = models.BooleanField(default=False, editable=False)
+
+    def __str__(self) :
+        return f"Le client {self.client} a été enregristré par {self.created_by} sur la liste d'attente"
+    
+class SuppervisionTravaux(models.Model):
+    id = models.AutoField(primary_key=True)
+    client = models.ForeignKey(Client, null=True, on_delete=models.PROTECT)
+    budget1 = models.ForeignKey(Paiement, on_delete=models.PROTECT)
+    #image = 
+    description = models.CharField(max_length=500,null=True) 
+
+
+    def __str__(self) :
+        return f"La description des travaux du client {self.client} a été faite"
+    
+class RenovationFaite(models.Model):
+    id = models.AutoField(primary_key=True)
+    client = models.ForeignKey(Client, null=True, on_delete=models.PROTECT)
+    budget2 = models.ForeignKey(Paiement,  on_delete=models.PROTECT)  
+    #image = 
+    description = models.CharField(max_length=500,null=True)  
+
+    def __str__(self) :
+        return f"La description des fins de travaux du client {self.client} a été validé"
+# Create your models here.
