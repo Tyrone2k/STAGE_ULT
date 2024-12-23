1cbefc8d5ff5ef6feb1d34e58800d57f29d34ed1
Tyrone2k <gastephane0@gmail.com>
2024-12-23 21:49:15 +0200

    commit page connection et navbar

--- Suppervision/urls.py
+++ Suppervision/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import*
 from django.contrib.auth.views import LoginView,LogoutView
+from . import views
 
 urlpatterns = [
     path('',home_view,name='home'),
     path('signin/',signin, name="signin" ),
     path('client-home/', client_home_view,name='client-home'),
-    path('login/', LoginView.as_view(template_name='Suppervision/login.html'),name='login'),
-    path('afterlogin/', afterlogin_view,name='afterlogin'),
+    path('login/', views.login, name='login'),
+    path('services/', views.services, name='services'),
 ]
--- Suppervision/views.py
+++ Suppervision/views.py
@@ -6,18 +6,28 @@ from django.contrib.auth.decorators import login_required,user_passes_test
 
 
 # Create your views here.
-def home_view(request):
-    designs=models.Design.objects.all()
-    if 'id_design' in request.COOKIES:
-        id_design = request.COOKIES['id_design']
-        compter = id_design.split('|')
-        compter_design_panier = len(set(compter))
-    else:
-        compter_design_panier = 0
-    if request.user.is_authenticated:
-        return HttpResponseRedirect('afterlogin')    
 
-    return render(request,'home.html',{'designs':designs, 'compter_design_panier':compter_design_panier})
+
+
+def login(request):
+    return render(request,'login.html')
+
+
+
+
+def home_view(request):
+    # designs=models.Design.objects.all()
+    # if 'id_design' in request.COOKIES:
+    #     id_design = request.COOKIES['id_design']
+    #     compter = id_design.split('|')
+    #     compter_design_panier = len(set(compter))
+    # else:
+    #     compter_design_panier = 0
+    # if request.user.is_authenticated:
+    #     return HttpResponseRedirect('afterlogin')    
+
+    # return render(request,'home.html',{'designs':designs, 'compter_design_panier':compter_design_panier})
+    return render (request, 'home.html')
 
 def signin(request):
     userForm=forms.ClientUserForm()
@@ -42,11 +52,11 @@ def signin(request):
 def is_customer(user):
     return user.groups.filter(name='CLIENT').exists()
 
-def afterlogin_view(request):
-    if is_customer(request.user):
-        return redirect('client-home')
-    else:
-        return redirect('home')
+# def afterlogin_view(request):
+#     if is_customer(request.user):
+#         return redirect('client-home')
+#     else:
+#         return redirect('home')
     
     
 @login_required(login_url='login')
@@ -64,3 +74,8 @@ def client_home_view(request):
 
     return render(request,'home.html',{'designs':designs, 'compter_design_panier':compter_design_panier})    
     
+
+
+
+def services(request):
+    return render(request,'services.html')
